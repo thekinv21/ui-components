51b1d6064369ee605e29e5741f5143005990115a
Vadim Kiniabaev <thekinv21@gmail.com>
2024-09-27 21:22:19 +0300

    FEAT:[Modal][Vadim]: fix modal size error

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -18,6 +18,25 @@ interface IModal {
 }
 
 export function Modal({ children, size = 'md', ...props }: IModal) {
+	const sizeChange = (size: string): string => {
+		switch (size) {
+			case 'xs':
+				return `max-w-xs`
+			case 'sm':
+				return `max-w-sm`
+			case 'md':
+				return `max-w-md`
+			case 'lg':
+				return `max-w-lg`
+			case 'xl':
+				return `max-w-xl`
+			case '2xl':
+				return `max-w-2xl`
+			default:
+				return 'max-w-md'
+		}
+	}
+
 	return (
 		<Dialog
 			as='div'
@@ -28,7 +47,9 @@ export function Modal({ children, size = 'md', ...props }: IModal) {
 			<DialogBackdrop className='fixed inset-0 z-10 overflow-y-auto bg-[black]/50'>
 				<div className='flex min-h-full items-center justify-center p-4 text-center'>
 					<DialogPanel
-						className={`animate__animated ${props.animate ? props.animate : 'animate__fadeIn'} w-full transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all ${size ? `max-w-${size}` : 'max-w-md'}`}
+						className={`animate__animated ${props.animate ? props.animate : 'animate__fadeIn'} w-full transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all ${sizeChange(
+							size
+						)}`}
 					>
 						<div className='mt-4'>{children}</div>
 					</DialogPanel>
