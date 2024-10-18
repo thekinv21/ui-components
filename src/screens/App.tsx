279d8dd681a9f0ce994a32a14353cbcb5c4998bb
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-18 21:32:10 +0300

    FİX:[master-xxx][Vadim]: app tsx değiştirildi

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,5 +1,7 @@
 import { PaginationState } from '@tanstack/react-table'
+import { Fragment } from 'react/jsx-runtime'
 
+import { Footer3 } from '@/components/layout/footer/footer-3/Footer3'
 import { BreadCrumbExample3 } from '@/components/ui/breadcrumb/example-3/BreadCrumbExample3'
 import { DataTable, useDataTableColumns } from '@/components/ui/dataTable'
 
@@ -19,30 +21,34 @@ export function App() {
 	}
 
 	return (
-		<section className='container mx-auto w-full py-20'>
-			<div className='space-y-10'>
-				<BreadCrumbExample3
-					array={[
-						{
-							key: 'components',
-							url: '#'
-						},
-						{
-							key: 'UI DataTable',
-							url: '#'
-						}
-					]}
-				/>
-
-				<div className='flex flex-col gap-5'>
-					<DataTable
-						tableHeading='Haberler Listesi'
-						customQueryKey={QueryKeyEnum.GET_ALL_NEWS}
-						customQueryFn={getNews}
-						columns={columns}
+		<Fragment>
+			<section className='container mx-auto w-full py-20'>
+				<div className='space-y-10'>
+					<BreadCrumbExample3
+						array={[
+							{
+								key: 'components',
+								url: '#'
+							},
+							{
+								key: 'UI DataTable',
+								url: '#'
+							}
+						]}
 					/>
+
+					<div className='flex flex-col gap-5'>
+						<DataTable
+							tableHeading='Haberler Listesi'
+							customQueryKey={QueryKeyEnum.GET_ALL_NEWS}
+							customQueryFn={getNews}
+							columns={columns}
+						/>
+					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+
+			<Footer3 />
+		</Fragment>
 	)
 }
