d39fc7ef4a15377c7c387a74b5080c0426da486e
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-19 16:15:56 +0300

    fix:[master-xxx][Vadim]:  Button hataları düzeltildi

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 
-import { cn } from '@/utils/shadcn'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { Loader } from 'lucide-react'
+
+import { cn } from '@/utils/shadcn'
 
 const buttonVariants = cva(
 	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -18,23 +19,27 @@ const buttonVariants = cva(
 					'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 transition-colors ease-in-out duration-200',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
 				link: 'text-primary transition-colors ease-in-out duration-200 underline-offset-4 hover:underline',
+
+				success:
+					'bg-green-600 text-primary-foreground shadow hover:bg-green-600/90 transition-colors ease-in-out duration-200',
+
 				danger:
 					'bg-red-600 transition-colors ease-in-out duration-200 text-white shadow-sm hover:bg-red-500/90',
 				info: 'bg-blue-400 text-white transition-colors ease-in-out duration-200 shadow-sm hover:bg-blue-500',
 				warning:
-					'bg-orange-400 text-white shadow-sm hover:bg-orange-500 transition-colors ease-in-out duration-200',
+					'bg-orange-400 text-white shadow-sm hover:bg-orange-500 transition-colors ease-in-out duration-200'
 			},
 			size: {
 				default: 'h-9 px-4 py-2',
 				sm: 'h-8 rounded-md px-3 text-xs',
 				lg: 'h-10 rounded-md px-8',
-				icon: 'h-9 w-9',
-			},
+				icon: 'h-9 w-9'
+			}
 		},
 		defaultVariants: {
 			variant: 'default',
-			size: 'default',
-		},
+			size: 'default'
+		}
 	}
 )
 
