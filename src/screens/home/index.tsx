300a5f591d0843960045c9aab1d37fb6b6dcae90
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-01 14:13:04 +0300

    fix:[master-xxx][Vadim]: fixed ubuntu build error

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,25 +1,6 @@
-import { PaginationState } from '@tanstack/react-table'
-
 import { BreadCrumbExample3 } from '@/components/ui/breadcrumb/example-3/BreadCrumbExample3'
 
-import { DataTable } from '../../components/ui/dataTable/DataTable'
-import { useDataTableColumns } from '../../components/ui/dataTable/useDataTableColumns'
-
-import { QueryKeyEnum } from '@/constant/queryKey.constant'
-import { baseService } from '@/service/base.service'
-
 export default function HomePage() {
-	const { columns } = useDataTableColumns()
-
-	const getNews = async (p: PaginationState) => {
-		const response = await baseService.getAll({
-			page: p.pageIndex,
-			pageSize: p.pageSize
-		})
-
-		return response?.data
-	}
-
 	return (
 		<section className='container mx-auto w-full py-20'>
 			<div className='space-y-10'>
@@ -30,19 +11,14 @@ export default function HomePage() {
 							url: '#'
 						},
 						{
-							key: 'UI DataTable',
+							key: 'UI Home',
 							url: '#'
 						}
 					]}
 				/>
 
 				<div className='flex flex-col gap-5'>
-					<DataTable
-						tableHeading='Haberler Listesi'
-						customQueryKey={QueryKeyEnum.GET_ALL_NEWS}
-						customQueryFn={getNews}
-						columns={columns}
-					/>
+					<h1 className='text-center text-2xl font-light'>Coming Son</h1>
 				</div>
 			</div>
 		</section>
